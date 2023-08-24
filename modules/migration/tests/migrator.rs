d81e0a080f2cab25ad58993344870416405c86cc
Lyrise <lyrise1984@gmail.com>
2023-08-24 21:24:41 +0900

    fix

--- modules/migration/tests/migrator.rs
+++ modules/migration/tests/migrator.rs
@@ -3,7 +3,9 @@
 mod tests {
     use omnius_core_migration::Migrator;
     use omnius_core_testkit::containers::postgres::PostgresContainer;
+    use serial_test::serial;
 
+    #[serial(migrate)]
     #[tokio::test]
     async fn simple_create_table_test() {
         let docker = testcontainers::clients::Cli::default();
@@ -21,6 +23,7 @@ mod tests {
         migrator.migrate().await.expect("Migrator migrate error");
     }
 
+    #[serial(migrate)]
     #[tokio::test]
     async fn create_table_syntax_error_test() {
         let docker = testcontainers::clients::Cli::default();
@@ -38,6 +41,7 @@ mod tests {
         migrator.migrate().await.expect_err("Error expected but successful.");
     }
 
+    #[serial(migrate)]
     #[tokio::test]
     async fn migrate_twice_test() {
         let docker = testcontainers::clients::Cli::default();
