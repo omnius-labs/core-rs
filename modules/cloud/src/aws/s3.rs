969cc3188619bd86a2082bbb5542283426ddb525
Lyrise <lyrise1984@gmail.com>
2024-01-26 08:59:28 +0900

    fix

--- modules/cloud/src/aws/s3.rs
+++ modules/cloud/src/aws/s3.rs
@@ -33,3 +33,29 @@ impl S3 for S3Impl {
         Ok(request.uri().to_string())
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use std::env;
+
+    use super::*;
+
+    #[ignore]
+    #[tokio::test]
+    async fn simple_test() {
+        env::set_var("AWS_PROFILE", "opxs-dev");
+        env::set_var("AWS_REGION", "us-east-1");
+        let sdk_config = aws_config::load_from_env().await;
+        let s3 = S3Impl {
+            client: aws_sdk_s3::Client::new(&sdk_config),
+        };
+        let uri = s3
+            .gen_put_presigned_uri("opxs.v1.dev.test", "test.txt", Utc::now(), Duration::minutes(5))
+            .await
+            .unwrap();
+        println!("{:?}", uri);
+        let client = reqwest::Client::new();
+        let res = client.put(&uri).body("test").send().await.unwrap();
+        println!("{:?}", res);
+    }
+}
