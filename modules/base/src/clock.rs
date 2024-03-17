1c8ca647aedc58ccd5eaaab4a08c8421b837d7ac
Lyrise <lyrise1984@gmail.com>
2024-03-17 18:25:28 +0900

    fix

--- modules/base/src/clock.rs
+++ modules/base/src/clock.rs
@@ -1,3 +1,8 @@
+use std::{
+    collections::VecDeque,
+    sync::{Arc, Mutex},
+};
+
 use chrono::{DateTime, TimeZone, Utc};
 
 pub trait SystemClock<Tz: TimeZone> {
@@ -9,5 +14,23 @@ pub struct SystemClockUtc;
 impl SystemClock<Utc> for SystemClockUtc {
     fn now(&self) -> DateTime<Utc> {
         Utc::now()
+    }
+}
+
+pub struct SystemClockUtcMock {
+    pub vs: Arc<Mutex<VecDeque<DateTime<Utc>>>>,
+}
+
+impl SystemClock<Utc> for SystemClockUtcMock {
+    fn now(&self) -> DateTime<Utc> {
+        self.vs.lock().unwrap().pop_front().unwrap()
+    }
+}
+
+impl SystemClockUtcMock {
+    pub fn new(vs: Vec<DateTime<Utc>>) -> Self {
+        Self {
+            vs: Arc::new(Mutex::new(VecDeque::from(vs))),
+        }
     }
 }
