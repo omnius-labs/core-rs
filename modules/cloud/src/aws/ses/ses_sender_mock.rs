bdb58cf98e2a6a67c8a256b2fc6238b0eaf57e65
Lyrise <lyrise1984@gmail.com>
2024-03-17 17:33:52 +0900

    fix

--- modules/cloud/src/aws/ses/ses_sender_mock.rs
+++ modules/cloud/src/aws/ses/ses_sender_mock.rs
@@ -1,15 +1,12 @@
-use std::cell::RefCell;
+use std::sync::{Arc, Mutex};
 
 use async_trait::async_trait;
 
 use super::SesSender;
 
 pub struct SesSenderMock {
-    pub send_mail_simple_text_inputs: RefCell<Vec<SendMailSimpleTextInput>>,
+    pub send_mail_simple_text_inputs: Arc<Mutex<Vec<SendMailSimpleTextInput>>>,
 }
-
-unsafe impl Sync for SesSenderMock {}
-unsafe impl Send for SesSenderMock {}
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct SendMailSimpleTextInput {
@@ -22,7 +19,7 @@ pub struct SendMailSimpleTextInput {
 #[async_trait]
 impl SesSender for SesSenderMock {
     async fn send_mail_simple_text(&self, to_address: &str, from_address: &str, subject: &str, text_body: &str) -> anyhow::Result<()> {
-        self.send_mail_simple_text_inputs.borrow_mut().push(SendMailSimpleTextInput {
+        self.send_mail_simple_text_inputs.lock().unwrap().push(SendMailSimpleTextInput {
             to_address: to_address.to_string(),
             from_address: from_address.to_string(),
             subject: subject.to_string(),
@@ -36,7 +33,7 @@ impl SesSenderMock {
     #[allow(unused)]
     pub fn new() -> Self {
         Self {
-            send_mail_simple_text_inputs: RefCell::new(vec![]),
+            send_mail_simple_text_inputs: Arc::new(Mutex::new(vec![])),
         }
     }
 }
