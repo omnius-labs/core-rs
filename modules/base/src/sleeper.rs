ed935ef5575bd14c830d1a7e515bda82f9b8bc78
Lyrise <lyrise1984@gmail.com>
2024-04-25 00:32:37 +0900

    fix

---
+++ modules/base/src/sleeper.rs
@@ -0,0 +1,37 @@
+use std::time::Duration;
+
+use async_trait::async_trait;
+
+#[async_trait]
+pub trait Sleeper {
+    async fn sleep(&self, duration: Duration);
+}
+
+pub struct RealSleeper;
+
+#[async_trait]
+impl Sleeper for RealSleeper {
+    async fn sleep(&self, duration: Duration) {
+        tokio::time::sleep(duration).await;
+    }
+}
+
+pub struct FakeSleeper;
+
+#[async_trait]
+impl Sleeper for FakeSleeper {
+    async fn sleep(&self, _duration: Duration) {
+        tokio::task::yield_now().await;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    async fn test_do_something() {
+        let fake_sleeper = FakeSleeper;
+        fake_sleeper.sleep(Duration::from_secs(1)).await;
+    }
+}
