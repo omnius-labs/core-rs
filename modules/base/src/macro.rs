c9f1a93d5524a84b80684c118ed89e1f9fa08123
Lyrise <lyrise1984@gmail.com>
2025-04-06 21:42:23 +0900

    fix

--- modules/base/src/macro.rs
+++ modules/base/src/macro.rs
@@ -10,3 +10,13 @@ macro_rules! hook_err {
         }
     }};
 }
+
+#[macro_export]
+macro_rules! ensure_err {
+    ($condition:expr, $callback:expr) => {{
+        if !($condition) {
+            let e = ($callback)();
+            return Err(e);
+        }
+    }};
+}
