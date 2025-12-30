7d1477e87ab027bb7efac3a20426077764290457
Lyrise <lyrise1984@gmail.com>
2025-12-31 02:12:25 +0900

    fix

--- modules/omnikit/src/lib.rs
+++ modules/omnikit/src/lib.rs
@@ -1,12 +1,8 @@
 mod error;
 pub mod model;
 mod prelude;
+mod result;
 pub mod service;
-
-mod result {
-    #[allow(unused)]
-    pub type Result<T> = std::result::Result<T, crate::error::Error>;
-}
 
 pub use error::*;
 pub use result::*;
